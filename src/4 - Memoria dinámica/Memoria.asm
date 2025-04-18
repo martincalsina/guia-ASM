6028418e23b3bf0bca9793cf2fa701467c2ecead
martincalsina <martincalsina05@gmail.com>
2025-04-18 09:50:42 -0300

    ej 4 compStr

--- src/4 - Memoria dinámica/Memoria.asm
+++ src/4 - Memoria dinámica/Memoria.asm
@@ -15,8 +15,48 @@ global strLen
 ; ** String **
 
 ; int32_t strCmp(char* a, char* b)
+; a --> RDI
+; b --> RSI ambos son punteros, 64 bits.
 strCmp:
-	ret
+
+	mov DL, byte [RDI + 0] ;primer caracter de a
+	mov CL, byte [RSI + 0] ;primer caracter de b
+	
+	.loop_while:
+		cmp DL, CL ;los char ocupan 1 byte, debo compararlos asi
+		jg .end_a_grater_than_b
+		jl .end_a_lower_than_b
+		mov R9B, DL
+		or R9B, CL ;combino los bits del caracter a y b que estoy leyendo en R9
+		test R9B, R9B ;chequeo si ambos eran 0 al mismo tiempo, solo me importa el byte de abajo pq es un char
+		je .end_a_equals_b 
+
+		;si no pasa nada de eso, puedo seguir buscando caracteres
+		add RDI, 1 ;incremento el puntero de a
+		add RSI, 1 ;lo mismo con el de b
+
+		;tomo los nuevos caracteres
+		mov DL, byte [RDI]
+		mov CL, byte [RSI]
+
+		jmp .loop_while
+
+
+
+	.end_a_grater_than_b:
+		mov EAX, -1
+		jmp .end
+	
+	.end_a_lower_than_b:
+		mov EAX, 1
+		jmp .end
+	
+	.end_a_equals_b:
+		xor EAX, EAX
+		jmp .end
+	
+	.end:
+		ret
 
 ; char* strClone(char* a)
 strClone:
